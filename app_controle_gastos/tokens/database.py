5c58c2cf6c56a8b62c41f414b1ade62c154e8865
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-09-04 09:45:14 -0300

    Resolvido bug de conexão com o banco de dados

--- app_controle_gastos/tokens/database.py
+++ app_controle_gastos/tokens/database.py
@@ -4,7 +4,7 @@ class KeysDB(configparser.ConfigParser):
     def __init__(self, tipo_config: str='secret') -> None:
         super().__init__()
         self.read(f'app_controle_gastos/{tipo_config}_config.ini')
-        self.USERS = self.get('DB', 'user')
+        self.USER = self.get('DB', 'user')
         self.HOST = self.get('DB', 'host')
         self.PASSWORD = self.get('DB', 'passwd')
         self.PORT = self.get('DB', 'port')
