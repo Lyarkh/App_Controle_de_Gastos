2910f9966aeadc6241d957b9c850390aef8675f8
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-01-02 22:06:07 -0300

    Refatoração da parte do front end
    Parte do frontend será feito em react

--- app_controle_gastos/frontend/tela_inicial.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import  QWidget
-
-
-class TelaInicial(QWidget):
-    def __init__(self) -> None:
-        super().__init__()
-        uic.loadUi("app_controle_gastos/frontend/layouts/tela_inicial.ui", self)
