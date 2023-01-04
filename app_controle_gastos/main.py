bf59b59da0202d46d7d82e7eb809c3ba4d94d115
Lucas Emanuel <82418777+Lyarkh@users.noreply.github.com>
2023-01-03 22:00:24 -0300

    Modificando classe main
    Mudanças devido a requisitos e refatoração do frontend

--- app_controle_gastos/main.py
+++ app_controle_gastos/main.py
@@ -1,10 +1,8 @@
-from app_controle_gastos.frontend.central_windows import CentralWindows
 
 
 class Main:
-    @staticmethod
     def run() -> None:
-        CentralWindows().tela_inicial()
+        ...
 
 
 if __name__ == '__main__':
